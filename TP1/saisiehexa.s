b793db50718943129a41229d569a549fe0a1532d
Alex <alex@Debian.fr>
2016-01-31 12:41:21 +0100

    Début de fusion des programmes (exo 1 et exo 2 fait)

--- TP1/saisiehexa.s
+++ TP1/saisiehexa.s
@@ -1,57 +1,95 @@
 .data
-UnNombre:
+NombreSaisie:
     .space 10
+TailleMax:
+    .quad 10
+TailleSaisie:
+    .quad 0
 .text
 .globl _start
 _start:
+
+/* Saisie */
+saisie:
     movq $3 , %rax
     movq $0 , %rbx
-    movq $10 , %rdx
-    movq $UnNombre , %rcx
+    movq $NombreSaisie , %rcx
+    movq TailleMax , %rdx
     int $0x80
-    
-    movq $0 , %rdx
-    movb UnNombre , %dl
-    movq $UnNombre , %rcx
-    
+
     movq $0 , %rax
-    movq $48 , %r10
-    movq $10 , %r11
+    movq $0 , %rdx
+    movb NombreSaisie , %dl
+    movq $NombreSaisie , %rcx
 
-while:
-    cmp $10 , %rdx
-    je prepareToString
-    cmp $0 , %rdx
-    je prepareToString
+    /* Si on commence par un retour chariot ou marqueur de fin */
+    cmp $'.' , %rdx
+    je fin
+    cmp $'\n' , %rdx
+    je fin
 
-    movq %rdx , %r9
+verif:
+    /*Si le caractère est supérieur à 64 c'est une lettre */
+    cmp $'A' , %rdx
+    jge lettre
 
-    imul %r11
-    sub %r10 , %r9
-    add %r9 , %rax
+chiffre:
+    /* Si le caractère est inférieur à 48 (caractère '0'), on renvoie l'utilisateur à la saisie */
+    cmp $'0' , %rdx
+    jl saisie
+
+    /* Si le caractère est supérieur à 57 (caractère '9'), on renvoie l'utilisateur à la saisie */
+    cmp $'9' , %rdx
+    jg saisie
+
+    /* traduit le premier caractère en un entier hexadecimal */
+    subb $'0' , %dl
+    jmp inc
+
+/* Pour chacun des trois cas suivant on soustrait la valeur du code ascii 
+ * du point 0 (48 pour les chiffres, 55 pour les majuscules et 87 pour 
+ * les minuscules) afin d'obtenir la bonne valeur hexadécimale */
+
+lettre:
+    /* Si le caractère est supérieur à 102 (caractère 'f'), on renvoie l'utilisateur à la saisie */
+    cmp $'f' , %rdx
+    jg saisie
+
+    /* Si le caractère est inférieur à 97 (caracctère 'a'), c'est peut-être une majuscule */
+    cmp $'a' , %rdx
+    jl lettreMajuscule
+    
+    subb $87 , %dl
+    jmp inc
+
+lettreMajuscule:
+    /* Si le caractère est supérieur à 70 (caractère 'F'), on renvoie l'utilisateur à la saisie */
+    cmp $'F' , %rdx
+    jg saisie
+
+    subb $55 , %dl
+
+inc:
+    /* Ou binaire pour concaténer le chiffre hexa en cours de lecture au nombre final*/
+    orb %dl , %al
+    incq TailleSaisie
 
     incq %rcx
     movq $0 , %rdx
-    movb (%rcx) , %dl
-    jmp while
+    movb (%rcx), %dl
 
-prepareToString:
-    movq $10,%rdx
+    /* Vérification si la fin de chaine a été atteinte */
+    cmp $'.' , %rdx
+    je fin
+    cmp $'\n' , %rdx
+    je fin
 
-toString:
-    cmp $0 , %rax
-    je end
-    
-    shr $2, %rdx 
-    movq %rdx,%r9
+    /* décalage de l'entier vers la gauche pour pouvoir concaténer la prochaine valeur */
+    shl $4 , %rax
+    jmp verif
 
-    idiv %r11
-    add %r10, %rdx
-
-    or %r9,%rdx
-    jmp toString
-
-end:
+/* Fin */
+fin:
     movq $0 , %rbx
     movq $1 , %rax
     int $0x80
