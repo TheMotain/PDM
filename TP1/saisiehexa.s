63127fd4d12be044b9fd465d262951f734ccf91e
Alex <alex@Debian.fr>
2016-01-31 19:29:44 +0100

    Programme complet il reste un bug sur l'affichage il manque le premier caractère

--- TP1/saisiehexa.s
+++ TP1/saisiehexa.s
@@ -1,8 +1,16 @@
 .data
+/* Buffer d'entrée */
 NombreSaisie:
     .space 10
+/* Buffer de sortie */
+NombreAffichage:
+    .rept 10
+    .quad 0
+    .endr
+/* Taille Max du Buffer */
 TailleMax:
     .quad 10
+/* Taille de la Saisie */
 TailleSaisie:
     .quad 0
 .text
@@ -24,9 +32,9 @@ saisie:
 
     /* Si on commence par un retour chariot ou marqueur de fin */
     cmp $'.' , %rdx
-    je fin
+    je transformation_ascii
     cmp $'\n' , %rdx
-    je fin
+    je transformation_ascii
 
 verif:
     /*Si le caractère est supérieur à 64 c'est une lettre */
@@ -80,13 +88,88 @@ inc:
 
     /* Vérification si la fin de chaine a été atteinte */
     cmp $'.' , %rdx
-    je fin
+    je transformation_ascii
     cmp $'\n' , %rdx
-    je fin
+    je transformation_ascii
 
     /* décalage de l'entier vers la gauche pour pouvoir concaténer la prochaine valeur */
     shl $4 , %rax
     jmp verif
+
+/* transformation de l'entier en une chaine de charactères ascii*/
+transformation_ascii:
+    /* initialisation de la chaine ascii en partant par la fin */
+    /* curseur */
+    movq $1 , %r8
+    movq $NombreAffichage , %rcx
+    movq $'\n' , (%rcx)
+    incq %rcx
+
+transformation:
+    /* La lecture de l'entier est elle finie */
+    cmp $0 , %rax
+    je inverse_tableau
+    
+    /* mise en place d'un masque pour récupérer l'entier exa chiffre par chiffre*/
+    movb $0x0f , %bl
+    andb %al , %bl
+    /* Si le chiffre hexa est un chiffre ou une lettre */
+    cmp $9 , %bl
+    jg transformation_lettre
+    jmp transformation_chiffre
+
+/* ajout de l'entier correspondant au caractère ascii */
+transformation_lettre:
+    movb $55 , %r9b
+    jmp ascii
+
+transformation_chiffre:
+    movb $'0' , %r9b
+
+/* ajout du caractère ascii calculé dans la chaine de caractère */
+ascii:
+    addb %r9b , %bl
+    movq %rbx , (%rcx)
+    incq %rcx
+    /* incrémentation du curseur et déplacement de l'entier afin de prendre les 4 bits suivants */
+    incq %r8
+    shr $4 , %rax
+    jmp transformation
+
+/* le tableau de caractère étant inversé il nécessite d'être remis dans le bon sens */
+inverse_tableau:
+    movq $NombreAffichage , %rcx
+    movq $1 , %rdx
+
+/* On enpile tous les caractères du tableau jusque atteindre la fin de la chaine */
+empile_tableau:
+    cmp %r8 , %rdx
+    je pre_depile_tableau
+    push (%rcx)
+    incq %rcx
+    incq %rdx
+    jmp empile_tableau
+
+/* On repose le tableau au début */
+pre_depile_tableau:
+    movq $NombreAffichage , %rcx
+
+/* on dépile le tableau jusqu'à ce que la chaine soit reconstruite */
+depile_tableau:
+    cmp $0 , %rdx
+    je affichage
+    pop (%rcx)
+    incq %rcx
+    decq %rdx
+    jmp depile_tableau
+
+/* Affichage de l'entier */
+affichage:
+    movq $4 , %rax
+    movq $1 , %rbx
+    movq $NombreAffichage , %rcx
+    movq TailleSaisie , %rdx
+    int $0x80
 
 /* Fin */
 fin:
